8d01b7a7a470743a06310f9e33cbe7ad24114439
Jake Higgott <jakehiggott1@gmail.com>
2021-04-29 00:40:47 -0400

    <bot knows if people have joined/left the server>

--- bot.py
+++ bot.py
@@ -7,5 +7,15 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Bot is ready.')
 
-client.run('INSERT YOUR BOT CER HERE') #MAKE SURE YOU DO NOT COMMIT YOUR CER
+#Anytime the bot sees that someone has joined a server it is connected to it will announce that they have joined
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+#Anytime someone leaves or joins the server it will say it.
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
+client.run('BOT TOKEN HERE') #MAKE SURE YOU DO NOT COMMIT YOUR CER
 
