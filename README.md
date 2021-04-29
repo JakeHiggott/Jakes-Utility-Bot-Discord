0cbe6ce7bfa54a3a82d975790e9d397378195338
Jake Higgott <jakehiggott1@gmail.com>
2021-04-29 01:39:30 -0400

    Added 8 ball and latency commands see readme

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 JAKES UTILITY BOT FOR DISCORD
 
-Functionality Includes:
+`Functionality Includes:`
 
--Can detect when someone has joined/left the server
+- Can detect when someone has joined/left the server
+
+- You can play 8 Ball with it
+
+        Command: !8ball YOUR_QUESTION
+
+- It can tell you what its connection latency is
+
+        Command: !ping
 
--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random 
 
 client = commands.Bot(command_prefix = '.')
 
@@ -19,7 +20,41 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left a server.')
+    
+
+#commands bot will do something
+@client.command()
+
+#this command will tell us the ping of the bot called with ".ping"
+async def ping(ctx):
+    await ctx.send(f'ping in ms: {round(client.latency * 1000)}ms ')
+
+#this command lets you roll and 8 ball and ask a question called with ".8ball YOUR QUESTION"
+@client.command(aliases=['8ball','8Ball'])
+async def _8Ball(ctx,*,question):
+    responses = ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
-client.run('INSERT TOKEN HERE') #I accidentally committed  this one time but it has since been reset :)
+
+client.run('YOUR CER HERE') #I accidentally committed  this one time but it has since been reset :)
 
